97fbcf367a3d4a910c973645daebd6b6c6cb31bb
huhaoda <huhaoda@shouqianba.com>
2023-11-08 16:39:58 +0800

    chore: 初始化

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+  import TailwindCss from './TailwindCSS.svelte';
+
+  let name = '雷猴'
+
+  function handleClick() {
+    name = '鲨鱼辣椒'
+  }
+</script>
+<TailwindCss />
+
+<div class="bg-red-400">Hello {name}</div>
+<button class="bg-gray" on:click={handleClick}>改名</button>
+<span class="ml-3 text-red-500">ABC Company</span>
+
